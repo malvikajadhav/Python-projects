048d5abd3cd6a0b3c98fa573105153a5249fc54e
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-06-23 18:40:51 -0400

    Band Name Generator
    Day 1/100

---
+++ Day1/Band_Name_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator")
+
+city = input("What is the name of your city?\n")
+
+name = input("What is the name of your pet?\n")
+
+print("Try the Band Name: "+ city + " " + name)

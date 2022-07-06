216662cfe147b657ca29612db4872258dec82887
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-05 23:12:22 -0400

    weeks of life
    https://waitbutwhy.com/2014/05/life-weeks.html

---
+++ Day2/Tim_urban_weeks_of_life.py
@@ -0,0 +1,6 @@
+age = input("What is your age right now?")
+years = 90 - int(age)
+months = 12*years
+weeks = 52*years
+days = 365*years
+print(f"You have {days} days, {weeks} weeks and {months} months left")

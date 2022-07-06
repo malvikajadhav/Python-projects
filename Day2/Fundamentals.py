7f3f751a98e0294303e367dfb7176c91e14a019d
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-05 22:53:52 -0400

    Day 2 lessons
    f-string
    underscore seperators
    string slicing by indexing

---
+++ Day2/Fundamentals.py
@@ -0,0 +1,16 @@
+print("Heloo"[3])
+print("123"+"456")
+#num_char = len(input("What is your name?"))
+#print("Your name has " + str(num_char) + " characters")
+val = input("What is your number?")
+unit = int(val)%10
+tens = int(val)/10
+print(unit+int(tens))
+sum_val = int(val[0]) + int(val[1])
+print(sum_val)
+floor_val = 8//3
+print(floor_val)
+cute = 20
+cuter = 21.66
+cutest = True
+print(f"my string is {cute}, bute it can be {cuter}, although I wish it was {cutest}")

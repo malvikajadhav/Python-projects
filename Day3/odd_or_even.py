b012d8280d6914a94437117d426832615d315184
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-07 22:40:39 -0400

    check odd even

---
+++ Day3/odd_or_even.py
@@ -0,0 +1,5 @@
+number = int(input("ENter the number you want to check: "))
+if number%2==0:
+    print("The entered number is a even number.")
+else: 
+    print("The entered number is a odd number.")

97171cc56a2010cae7843ac74ebc07538523d2fe
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-07 23:11:22 -0400

    Update Rollercoaster.py

--- Day3/Rollercoaster.py
+++ Day3/Rollercoaster.py
@@ -1,14 +1,21 @@
+
 print("Welcome to the rollercoaster!!")
 ht = int(input("What is your height in cms?"))
 
 if ht>=120:
     print("You can ride the rollercoaster!")
+    ticket = 0
     age = int(input("What is your age?"))
     if age<12:
-        print("Your ticket price is 5$")
+        ticket = 5
     elif 12<=age<18:
-        print("Your ticket price is 7$")  
+        ticket = 7 
     else:
-        print("Your ticket price is 12$")           
+        ticket = 12
+    answer = input("Do you want to buy the photos at the gift shop? Enter Yes or No")
+    if answer == "Yes":
+        print(f"Your total bill is {ticket + 3}$")
+    else:
+        print(f"Your total bill is {ticket}$")
 else:
     print("Sorry you have to grow taller before you can ride.")

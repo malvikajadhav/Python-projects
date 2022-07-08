980247e06bf7f7d1d5dbdd3d6c1051397861af90
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-07 22:34:34 -0400

    Roller coaster

---
+++ Day3/Rollercoaster.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!!")
+ht = int(input("What is your height in cms?"))
+
+if ht>=120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry you have to grow taller before you can ride.")

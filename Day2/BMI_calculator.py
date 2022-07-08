f3f05a6150c7cedc3739118b9b8dd16108edb8ec
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-07 22:52:55 -0400

    Update BMI_calculator.py

--- Day2/BMI_calculator.py
+++ Day2/BMI_calculator.py
@@ -1,4 +1,15 @@
 ht = input("ENter height: ")
 wt = input("ENter weight: ")
-bmi = int(float(wt)/(float(ht)**2))
-print(bmi)
+bmi = (float(wt)/(float(ht)**2))
+value = ""
+if bmi<=18.5:
+    value = "underweight"
+elif 18.5<bmi<=25:
+    value = "normal weight"
+elif 25<bmi<=30:
+    value = "overweight"
+elif 30<bmi<=35:
+    value = "obese"
+else:
+    value = "clinically obese"
+print(f"Your bmi is {round(bmi,2)}, you are {value}")

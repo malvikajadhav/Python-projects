a2cfe6d5d7f918e8fed47f35c51fa0a41f779705
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-07 22:45:05 -0400

    Update Rollercoaster.py

--- Day3/Rollercoaster.py
+++ Day3/Rollercoaster.py
@@ -3,5 +3,12 @@ ht = int(input("What is your height in cms?"))
 
 if ht>=120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+    if age<12:
+        print("Your ticket price is 5$")
+    elif 12<=age<18:
+        print("Your ticket price is 7$")  
+    else:
+        print("Your ticket price is 12$")           
 else:
     print("Sorry you have to grow taller before you can ride.")

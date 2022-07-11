f38475b1bb728809dd76a317b92f27331b7eb304
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-11 19:19:22 -0400

    Update Random.py

--- Day4/Random.py
+++ Day4/Random.py
@@ -1,1 +1,22 @@
+# https://en.wikipedia.org/wiki/Mersenne_Twister
+import random
+print(random.randint(1,10))
+print(random.random()*5)
+
+ls = random.randint(0,100)
+print(f"Your love score is {ls}")
+
+states = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(len(states))
+
+states.append("Austenland")
+
+print(len(states))
+
+states.extend(["Shloktopia","Malvikaland"])
+
+print(len(states))
+
+
 

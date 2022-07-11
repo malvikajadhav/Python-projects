43ccac4efdfcec2791514d6614161da124c61b85
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-11 18:56:59 -0400

    Treasure map

---
+++ Day4/Treasure_map.py
@@ -0,0 +1,8 @@
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+map[int(position[0])][int(position[1])] = 'X'
+print(f"{row1}\n{row2}\n{row3}")

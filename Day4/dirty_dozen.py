bf6a070eb38a5a0aa58a58c5d2432e664998984b
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-11 18:48:57 -0400

    DO NOT EAT
    https://www.delish.com/food-news/a26872638/dirty-dozen-foods-list-2019/

---
+++ Day4/dirty_dozen.py
@@ -0,0 +1,7 @@
+
+fruits = ["Strawberries","Spinach","Kale","Nectarines","Apples","Grapes","Peaches","Cherries","Pears","Tomatoes","Celery","Potatoes"]
+veggies = ["Avocados","Sweet corn","Pineapples","Cabbages","Onions","Sweet peas","Papayas","Asparagus","Mangoes","Eggplants","Honeydew melons","Kiwis","Cantaloupes","Cauliflower","Broccoli"]
+dirty_dozen = [fruits,veggies]
+
+print("How many dirty dozen veggies do we have ?")
+print(len(dirty_dozen[1]))

969c12148d418765792b14f5eb148f8a4c25c765
Malvika Jadhav <malvikajadhav99999@gmail.com>
2022-07-13 13:33:54 -0400

    list basics

---
+++ Day5/list.py
@@ -0,0 +1,15 @@
+fruits = ["Apple","Peach","Pear"]
+import numpy as np
+for fruit in fruits:
+    print(fruit)
+    print(f"{fruit} pie!")
+    
+sum = 0
+for num in range(0, 101, 2):
+    sum += num 
+print(sum)
+
+n = 4
+arr = np.arange(1,n+1)
+print(arr)
+print(str(arr))
